2d5b8a81ed03991f183c94eb792dd9037913ecca
Daniel Steuri <daniel.steuri@webrealisierung.ch>
2017-09-20 13:15:19 +0200

    Add hook to contao config

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,3 +4,7 @@ array_insert($GLOBALS['FE_MOD']['navigationMenu'],1, array(
         'wr-onepage' => 'ModuleWrOnepage'
     )
 );
+
+#Hooks
+$GLOBALS['TL_HOOKS']['getArticle'][] = array('wr.onepage.hooks', 'addAliasToArticleCssId');
+
