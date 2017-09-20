14f79bad7fa4b93050ad654d8fc50fd4f4fdbd3c
Daniel Steuri <daniel.steuri@webrealisierung.ch>
2017-09-20 13:16:06 +0200

    Add comments

--- src/WrOnepageBundle.php
+++ src/WrOnepageBundle.php
@@ -1,9 +1,18 @@
 <?php
 
+/**
+ * @copyright 2017 Webrealisierung GmbH
+ *
+ * @license LGPL-3.0+
+ */
+
 namespace Wr\OnepageBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * @author Daniel Steuri <mail@webrealisierung.ch>
+ */
 class WrOnepageBundle extends Bundle
 {
 }
