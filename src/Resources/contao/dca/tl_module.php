bf88643dc5570c02025e128805fc9bcf38d3a7c9
Daniel Steuri <daniel.steuri@webrealisierung.ch>
2017-10-02 21:50:38 +0200

    Add field

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -1,2 +1,12 @@
 <?php
-$GLOBALS['TL_DCA']['tl_module']['palettes']['wr-onepage-navigation'] = '{title_legend},name,type;{redirect},rootPage;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['wr-onepage-navigation'] = '{title_legend},name,type;{redirect_legend},rootPage;{options_legend},loadDefaultJavascript;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
+
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['loadDefaultJavascript'] = array
+(
+    'label'                   =>  &$GLOBALS['TL_LANG']['tl_module']['load_default_javascript'],
+    'exclude'                 => true,
+    'inputType'               => 'checkbox',
+    'eval'                    => array('tl_class'=>'w50'),
+    'sql'                     => "char(1) NOT NULL default ''"
+);
