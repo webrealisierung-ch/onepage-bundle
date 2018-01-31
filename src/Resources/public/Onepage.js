55c96438137f05248c27374eb49afb754e8c25a2
Daniel Steuri <daniel.steuri@webrealisierung.ch>
2018-01-31 21:08:34 +0100

    Refactor the Onepage.js

--- src/Resources/public/Onepage.js
+++ src/Resources/public/Onepage.js
@@ -1,161 +1,75 @@
-/**
- * @copyright Webrealisierung GmbH 2017
- * @license LGPL-3.0+
- *
- */
+function Onepage(Mover,selector){
 
-/**
- * Represent a one page object. The one page object provide
- * methods to scroll to a anchor element within a Page.
- * @param {string} [a[href*="#"]] selector - A optional valid css selector
- */
-
-function Onepage(selector){
-
-    if(selector === undefined){
-        this.a = document.querySelectorAll('a[href*="#"]');
-    } else{
-        this.a = document.querySelectorAll(selector+' a[href*="#"]');
+    if(Mover === undefined){
+        this.Mover = new Scroller();
+    }
+    else{
+        this.Mover = Mover;
     }
 
-    // Internal properties
-    this.scrollingElement =  document.scrollingElement || document.documentElement;
+    if(selector === undefined){
+        var a = document.querySelectorAll('a[href*="#"]');
+    } else{
+        var a = document.querySelectorAll(selector+' a[href*="#"]');
+    }
+
     this.offsetElement = 0;
 
-    // Config porperties
-    this.duration = 400;
-    this.easing = "linear";
-    this.offset = 0;
+    this.requiredMethods = [
+        'setDuration',
+        'getDuration',
+        'setEasing',
+        'getEasing',
+        'getPosition',
+        'move',
+        'movable',
+        'getQueue',
+        'abort'
+    ];
 
-    /**
-     * @constructor
-     */
-    this.init = function () {
-        var a = this.a;
+    var moveEvent = new Event('move');
 
-        for (var i = 0; a.length>i; i++) {
-            if(a[i].href){
-                anchorId=a[i].hash.replace("#", "");
-                var that = this;
-                if(this.checkIfIdExist(decodeURI(anchorId))){
+    this.init = function(){
+        try{
+            this.checkMoverMethods(this.Mover);
+
+            this.Mover.highlightActiveLink(a,moveEvent);
+
+            for (var i = 0; a.length>i; i++) {
+
+                if(a[i].href && this.Mover.movable(id) !== false){
+                    var onepage = this;
+                    var id = getHashFromLink(a[i]);
+
                     a[i].addEventListener("click",function(event){
-                        that.onClick(event,this,that);
+                        event.preventDefault();
+                        console.log(this);
+                        onepage.Mover.move(getHashFromLink(this));
                     });
                 }
             }
+
+
+        }
+        catch (e){
+            console.log(e);
         }
     };
 
-    /**
-     * Prevent the default click event of a given element and
-     * add the scroll function.
-     * @param event
-     * @param element
-     * @param self
-     */
-    this.onClick = function(event,element,self){
-        event.preventDefault();
-        anchorElement  = document.getElementById(decodeURI(element.hash.replace("#", "")));
-        var offsetTop = this.findBody(anchorElement);
-        self.scrollTo(this.scrollingElement, offsetTop);
+    getHashFromLink = function(link){
+        return decodeURI(link.hash.replace("#", ""));
     };
 
-    /**
-     * Todo: Implement the function
-     */
-    this.onScroll = function(){
+}
 
-    };
-
-    /**
-     * Check of a element with the given id is existing in the document
-     * @param {sting} id - Name of a css id
-     * @return {boolean} true if the element exist, otherwise false
-     */
-    this.checkIfIdExist = function (id) {
-        anchorElement=document.getElementById(id);
-        if(anchorElement){
-            return true;
-        }
-        return false
-    };
-
-    /**
-     * Scroll animation to a given Element
-     * @param element
-     * @param to
-     * @param startTime
-     */
-    this.scrollTo = function(element, to, startTime){
-        if( startTime===undefined ){
-            startTime = 0;
-            this.distance = Math.abs(to - element.scrollTop);
-            if(to >= element.scrollTop){
-                this.offsetElement = to - this.distance;
-                this.direction = "down";
-            } else {
-                this.offsetElement = element.scrollTop;
-                this.direction = "up";
+Onepage.prototype = {
+    constructor: Onepage,
+    checkMoverMethods: function(Mover){
+        for(i=0;this.requiredMethods.length > i;i++){
+            if(!Mover.hasOwnProperty(this.requiredMethods[i])){
+                throw 'Onepage Framework Error : Method '+this.requiredMethods[i]+' must be implemented!'
             }
         }
-        switch(this.easing){
-            case "linear":
-                t = this.linear(startTime/this.duration);
-                break;
-            case "easeIn":
-                t = this.easeIn(startTime/this.duration);
-                break;
-            case "easeOut":
-                t = this.easeOut(startTime/this.duration);
-                break;
-            case "easeInOut":
-                t = this.easeInOut(startTime/this.duration);
-                break;
-            default:
-                t = this.linear(startTime/this.duration);
-        }
-
-        scrollUnit = this.distance/1*t;
-
-        if(this.direction==="down"){
-            element.scrollTop = this.offsetElement + scrollUnit;
-        } else if(this.direction==="up"){
-            element.scrollTop = this.offsetElement - scrollUnit;
-        }
-
-        setTimeout(function(self){
-            if (startTime>=self.duration) return;
-            self.scrollTo(element, to, startTime + 5);
-        },5,this);
-    };
-
-    this.easeIn = function(t){
-        return t*t*t;
-    };
-
-    this.easeOut = function (t) {
-        return (--t)*t*t+1
-    };
-
-    this.easeInOut = function (t) {
-        return t<.5 ? 2*t*t : -1+(4-2*t)*t;
-    };
-
-    this.linear = function (t) {
-        return t;
-    };
-
-    this.findBody = function(element,offsetTop){
-
-        if(offsetTop===undefined){
-            offsetTop = - this.offset;
-        }
-
-        if(element.offsetParent != undefined)
-        {
-            offsetTop += element.offsetTop;
-            offsetTop = this.findBody(element.offsetParent, offsetTop);
-        }
-        return offsetTop;
+        return true;
     }
-}
+};
